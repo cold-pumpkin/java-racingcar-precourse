c485052a06353f7851f71a41610dba8a8f882938
Philip <pch4623@gmail.com>
2022-04-23 21:32:22 +0900

    feat(Input): add CarName creataed by user input car names
    Class for generating a valid car name object.

---
+++ src/main/java/racingcar/model/input/CarName.java
@@ -0,0 +1,29 @@
+package racingcar.model.input;
+
+public class CarName {
+
+    private static final int MIN_LENGTH = 1;
+    private static final int MAX_LENGTH = 5;
+
+    private static final String EMPTY_ERROR_MESSAGE = String.format("[ERROR] 자동차 이름은 %d자 이상 입력해주세요!", MIN_LENGTH);
+    private static final String OVER_LENGTH_ERROR_MESSAGE = String.format("[ERROR] 자동차 이름은 %d자 이하로 입력해주세요!", MAX_LENGTH);
+
+    private final String name;
+
+    public CarName(String name) {
+        validateCarName(name);
+        this.name = name;
+    }
+
+    private void validateCarName(String name) {
+        int nameLength = name.length();
+        if (nameLength < MIN_LENGTH )
+            throw new IllegalArgumentException(EMPTY_ERROR_MESSAGE);
+        if (nameLength > MAX_LENGTH)
+            throw new IllegalArgumentException(OVER_LENGTH_ERROR_MESSAGE);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
