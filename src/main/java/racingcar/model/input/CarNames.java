1e41afd72279d95b0ec597f10951e359b6e676ac
Philip <pch4623@gmail.com>
2022-04-24 17:49:51 +0900

    feat(CarNames): update constructor to create CarName instance when invoked

--- src/main/java/racingcar/model/input/CarNames.java
+++ src/main/java/racingcar/model/input/CarNames.java
@@ -1,11 +1,11 @@
 package racingcar.model.input;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CarNames {
-
     private static final int MIN_SIZE = 1;
     private static final int MAX_SIZE = 7;
 
@@ -13,11 +13,13 @@ public class CarNames {
     private static final String OVER_SIZE_ERROR_MESSAGE = String.format("[ERROR] %d대 이하의 자동차를 입력해주세요!", MAX_SIZE);
     private static final String NOT_UNIQUE_ERROR_MESSAGE = "[ERROR] 중복된 자동차 이름이 존재합니다!";
 
-    private final List<String> names;
+    private final List<CarName> carNames = new ArrayList<>();
 
     public CarNames(List<String> names) {
         validateCarNames(names);
-        this.names = names;
+        for (String name : names) {
+            carNames.add(new CarName(name));
+        }
     }
 
     public void validateCarNames(List<String> names) {
@@ -38,7 +40,7 @@ public class CarNames {
         return carNamesMap.size();
     }
 
-    public List<String> getNames() {
-        return names;
+    public List<CarName> getCarNames() {
+        return carNames;
     }
 }
