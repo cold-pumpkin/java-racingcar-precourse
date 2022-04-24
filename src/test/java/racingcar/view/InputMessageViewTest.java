e09144667e4a02692c7091bd74aa13eba37abef8
Philip <pch4623@gmail.com>
2022-04-24 21:48:30 +0900

    test(Input): extend the super class

--- src/test/java/racingcar/view/InputMessageViewTest.java
+++ src/test/java/racingcar/view/InputMessageViewTest.java
@@ -1,38 +1,20 @@
 package racingcar.view;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static racingcar.view.InputMessageView.printInputCarNamesMessage;
 import static racingcar.view.InputMessageView.printInputRacingRoundsMessage;
 
-class InputMessageViewTest {
-
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
-    @BeforeEach
-    public void setUp() {
-        System.setOut(new PrintStream(outputStreamCaptor));
-    }
-
-    @AfterEach
-    public void tearDown() {
-        System.setOut(standardOut);
-    }
+class InputMessageViewTest extends MessageViewTest {
 
     @Test
     @DisplayName("자동차 입력 안내 메시지가 정상적으로 출력되는지 확인한다.")
     void printInputCarNamesMessageTest() {
         printInputCarNamesMessage();
 
-        assertThat(outputStreamCaptor.toString().trim())
+        assertThat(output())
                 .isEqualTo("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     }
 
@@ -41,7 +23,7 @@ class InputMessageViewTest {
     void printInputRacingRoundsMessageTest() {
         printInputRacingRoundsMessage();
 
-        assertThat(outputStreamCaptor.toString().trim())
+        assertThat(output())
                 .isEqualTo("시도할 회수는 몇 회 인가요?");
     }
 
