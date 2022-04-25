2d4d12eae879d21481b853458c76b80a1f6e7c66
Philip <pch4623@gmail.com>
2022-04-25 19:59:42 +0900

    feat(View): add final winner message to show winners

--- src/main/java/racingcar/view/ResultMessageView.java
+++ src/main/java/racingcar/view/ResultMessageView.java
@@ -2,10 +2,15 @@ package racingcar.view;
 
 import racingcar.model.Car;
 import racingcar.model.Cars;
+import racingcar.model.Winner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResultMessageView {
     private static final String RESULT_START_MESSAGE = "실행 결과";
     private static final String PROGRESS_BAR = "-";
+    private static final String FINAL_WINNER_MESSAGE = "최종 우승자 : %s";
 
     public static void printResultStartMessage() {
         System.out.println(RESULT_START_MESSAGE);
@@ -22,5 +27,13 @@ public class ResultMessageView {
             System.out.print(PROGRESS_BAR);
         }
         System.out.println();
+    }
+
+    public static void printFinalWinnerMessage(Winner winner) {
+        List<String> winnerCarNames = new ArrayList<>();
+        winner.getWinners()
+                .forEach(car -> winnerCarNames.add(car.getName()));
+        System.out.printf(FINAL_WINNER_MESSAGE,
+                String.join(",", winnerCarNames));
     }
 }
