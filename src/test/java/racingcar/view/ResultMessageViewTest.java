d1839a595c49cd19ab2a1eb8a3964df712a38be1
Philip <pch4623@gmail.com>
2022-04-25 20:02:28 +0900

    test(View): final winner message test

--- src/test/java/racingcar/view/ResultMessageViewTest.java
+++ src/test/java/racingcar/view/ResultMessageViewTest.java
@@ -2,15 +2,16 @@ package racingcar.view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.model.Car;
 import racingcar.model.Cars;
+import racingcar.model.Winner;
 import racingcar.model.input.CarName;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static racingcar.view.ResultMessageView.printRaceProgressStatus;
-import static racingcar.view.ResultMessageView.printResultStartMessage;
+import static racingcar.view.ResultMessageView.*;
 
 class ResultMessageViewTest extends MessageViewTest {
 
@@ -36,6 +37,23 @@ class ResultMessageViewTest extends MessageViewTest {
                 .contains("park :")
                 .contains("kim :")
                 .contains("lee :");
+    }
+
+    @Test
+    @DisplayName("레이스 최종 우승자 안내 메시지가 정상적으로 출력되는지 확인한다.")
+    void printFinalWinnerMessageTest() {
+        List<CarName> carNames = Arrays.asList(
+                new CarName("park"), new CarName("kim"), new CarName("lee")
+        );
+        Cars cars = new Cars(carNames);
+        cars.move();
+
+        Winner winner = new Winner(cars);
+        printFinalWinnerMessage(winner);
+
+        assertThat(output())
+                .contains("최종 우승자 : ")
+                .containsAnyOf("park", "kim", "lee");
     }
 
 
